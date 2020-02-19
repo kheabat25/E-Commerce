512b75a29754898c886ccc3d469c25a087e49385
kheabat25 <kheabat@gmail.com>
2020-02-19 14:02:03 +0800

    update models

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb://localhost:27017/ecommerceOrders',
+  { useNewUrlParser: true },
+  err => {
+    if (!err) {
+      console.log('MongoDb connected');
+    } else {
+      console.log('error: ' + err);
+    }
+  }
+);
+
+require('./order.model');
---
+++ models/order.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var orderSchema = new mongoose.Schema({
+  order: {
+    type: String
+  },
+  total: {
+    type: String
+  }
+});
+
+mongoose.model('Order', orderSchema);
